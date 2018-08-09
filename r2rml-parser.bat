f4027b19fd1a73c91420c8f174396afe9cf7b5b8
carmenvandeloo <carmen_vandeloo@outlook.com>
2018-08-09 14:58:50 +0200

    upload dump to triple store

--- r2rml-parser.bat
+++ r2rml-parser.bat
@@ -1,4 +1,6 @@
 @echo off
-echo This is R2RML Parser 0.8-alpha. Run with -h for help on options.
+echo Transforming the data... Run with -h for help on options.
 java -Xms512m -Xmx4096m -cp "./*;./lib/*;" gr.seab.r2rml.beans.Main %1 %2
-echo R2RML Parser 0.8-alpha. Done.
+echo Transformation of data done.
+curl --digest --user dba:dba -vvv --url "http://localhost:8890/sparql-graph-crud-auth?graph-uri=https://data.vlaanderen.be/ns/wetgeving" -X POST -T dump.rdf
+echo Data uploaded into the triple store.
