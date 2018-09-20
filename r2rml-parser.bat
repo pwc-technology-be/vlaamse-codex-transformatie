7958c69181ff165c25e5e7687da4392ac57ad773
carmenvandeloo <carmen_vandeloo@outlook.com>
2018-09-20 13:50:50 +0200

    .ttl extension for dump

--- r2rml-parser.bat
+++ r2rml-parser.bat
@@ -2,5 +2,5 @@
 echo Transforming the data... Run with -h for help on options.
 java -Xms512m -Xmx4096m -cp "./*;./lib/*;" gr.seab.r2rml.beans.Main %1 %2
 echo Transformation of data done.
-curl --digest --user dba:dba -vvv --url "http://localhost:8890/sparql-graph-crud-auth?graph-uri=https://data.vlaanderen.be/ns/wetgeving" -X POST -T dump.rdf
+curl --digest --user dba:dba -vvv --url "http://localhost:8890/sparql-graph-crud-auth?graph-uri=https://data.vlaanderen.be/ns/wetgeving" -X POST -T dump.ttl
 echo Data uploaded into the triple store.
