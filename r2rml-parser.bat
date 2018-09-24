c071abfa1a07398035438237bdc13557c49f3ad2
carmenvandeloo <carmen_vandeloo@outlook.com>
2018-09-24 18:53:59 +0200

    use PUT instead of POST so that content is overwritten instead of added

--- r2rml-parser.bat
+++ r2rml-parser.bat
@@ -1,6 +1,9 @@
 @echo off
+@echo %time%
 echo Transforming the data... Run with -h for help on options.
-java -Xms512m -Xmx4096m -cp "./*;./lib/*;" gr.seab.r2rml.beans.Main %1 %2
+java -Xms512m -Xmx8g -cp "./*;./lib/*;" gr.seab.r2rml.beans.Main %1 %2
 echo Transformation of data done.
-curl --digest --user dba:dba -vvv --url "http://localhost:8890/sparql-graph-crud-auth?graph-uri=https://data.vlaanderen.be/ns/wetgeving" -X POST -T dump.ttl
+@echo %time%
+curl --digest --user dba:dba -vvv --url "http://localhost:8890/sparql-graph-crud-auth?graph-uri=https://data.vlaanderen.be/ns/wetgeving" -X PUT -T dump.ttl
 echo Data uploaded into the triple store.
+@echo %time%
